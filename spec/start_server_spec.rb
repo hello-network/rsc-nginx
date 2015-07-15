51f296b86e9b633fc30a0005f3d94aa6d45e502e
Richard Shade <rshade@rightscale.com>
2015-07-15 16:27:48 -0500

    adding stop and start

--- spec/start_server_spec.rb
+++ spec/start_server_spec.rb
@@ -1,1 +1,16 @@
 require_relative 'spec_helper'
+
+describe 'rsc-nginx::start_server' do
+
+  let(:chef_run) do
+    ChefSpec::Runner.new do |node|
+    end.converge(described_recipe)
+  end
+
+  context 'starts nginx' do
+    it 'will start nginx server' do
+      expect(chef_run).to start_service('nginx')
+    end
+  end
+
+end
--- spec/stop_server_spec.rb
+++ spec/stop_server_spec.rb
@@ -1,1 +1,16 @@
 require_relative 'spec_helper'
+
+describe 'rsc-nginx::stop_server' do
+
+  let(:chef_run) do
+    ChefSpec::Runner.new do |node|
+    end.converge(described_recipe)
+  end
+
+  context 'stopping nginx' do
+    it 'will stop nginx server' do
+      expect(chef_run).to stop_service('nginx')
+    end
+  end
+
+end

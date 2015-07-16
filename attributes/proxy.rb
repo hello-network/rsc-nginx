f3e5b495356db362032c1b86dd819cea09dab938
Richard Shade <rshade@rightscale.com>
2015-07-16 15:11:24 -0500

    addint proxy file

--- attributes/proxy.rb
+++ attributes/proxy.rb
@@ -3,7 +3,15 @@ default['nginx']['proxy']['location'] = [
   '/' =>
   {
     'proxy_cache_path' => '/tmp/nginx',
-    'proxy_pass' => 'http://127.0.0.1:8000'
+    'proxy_pass' => 'http://127.0.0.1:8000',
+    'param_file' => '/etc/nginx/proxy_params',
+    'params' =>
+      {
+        'Host' => '$http_host',
+        'X-Real-IP' => '$remote_addr',
+        'X-Forwarded-For' => '$proxy_add_x_forwarded_for',
+        'X-Forwarded-Proto' => '$scheme'
+      }
   }
 ]
 
@@ -12,6 +20,7 @@ default['nginx']['proxy']['cache']['proxy_cache_path']['/tmp/nginx'] =  {
     'keys_zone' => "#{node['nginx']['proxy']['proxy_cache']}:10m",
     'inactive' => '60m'
   }
+
 default['nginx']['proxy']['cache']['proxy_cache_key'] = '$scheme$request_method$host$request_uri'
 default['nginx']['proxy']['cache']['location'] =  [
     '\.(?:jpg|jpeg|gif|png|ico|cur|gz|svg|svgz|mp4|ogg|ogv|webm|htc)$' =>

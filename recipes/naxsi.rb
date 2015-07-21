dbc706ce35a775f7488049d51c0e43baded3a44a
Richard Shade <rshade@rightscale.com>
2015-07-21 15:28:30 -0500

    adding runstate

--- recipes/naxsi.rb
+++ recipes/naxsi.rb
@@ -15,3 +15,6 @@ end
 node.default['nginx']['default']['modules'] = @nginx_modules
 
 include_recipe "nginx::naxsi_module"
+
+node.run_state['nginx_configure_flags'] =
+  node.run_state['nginx_configure_flags'] | ["--add-module=#{naxsi_extract_path}/naxsi-#{node['nginx']['naxsi']['version']}/naxsi_src"]
